e699f72440f6b0259c0acd6923bdd408c450fa07
EddyBautista <bautista.j.eddy@gmail.com>
2021-03-13 07:04:46 -0600

    Added controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,8 +26,30 @@ namespace SaturdayMoring_CalcApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+
+        [HttpPost]
+        public ActionResult Calculate(string firstNumber, string secondNumber, string cal)
         {
+            int a = Convert.ToInt32(firstNumber);
+            int b = Convert.ToInt32(secondNumber);
+            int c = 0;
+
+            switch (cal)
+            {
+                case "Add":
+                    c = a + b;
+                    break;
+                case "Sub":
+                    c = a - b;
+                    break;
+                case "Mul":
+                    c = a * b;
+                    break;
+                case "Div":
+                    c = a / b;
+                    break;
+            }
+            ViewBag.Result = c;
             return View();
         }
 
